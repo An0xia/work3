0c52151df37f551e53dd044968ff05782789c05c
Alost1 <599761980@qq.com>
2021-10-18 19:14:55 +0800

    3-2

---
+++ src/checkISBN.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class checkISBN {
+    public static void main(String[] args){
+        System.out.print("Enter the first 9 digits of an ISBN as integer: ");
+        Scanner a=new Scanner(System.in);
+        int input=a.nextInt();
+        int sum=0;
+        int n=9;
+        int m=input;
+        while (input>0){
+            sum+=(input%10)*n;
+            input=input / 10;
+            n--;
+        }
+        System.out.print("The ISBN-10 number is 0");
+        System.out.print(m);
+        if((sum % 11)!=10)System.out.print(sum%11);
+        else System.out.print('X');
+    }
+}
