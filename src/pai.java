650ad2a5684dca9d7fade600beeff7e5b1bff673
syclover <syclover@yeah.net>
2021-10-29 15:22:42 +0800

    5-6

---
+++ src/pai.java
@@ -0,0 +1,11 @@
+public class pai {
+    public static void main(String[] args){
+        double pi=0;
+        for(int i=1;i<=100000;i++){
+            pi=pi+Math.pow(-1,i+1)/(2*i-1);
+            if(i%10000==0){
+                System.out.println(pi*4);
+            }
+        }
+    }
+}
